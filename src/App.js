194e3bb0302f7f4366ebe809085cda8203a0c4f0
Chris Halley <chris@chriss-mbp.home>
2019-08-09 16:23:19 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Everything's awesome!
         </a>
       </header>
     </div>
