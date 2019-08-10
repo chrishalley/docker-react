b4bdf374094beff2841238375ac10caf09c227b8
Chris Halley <chris@chriss-mbp.home>
2019-08-11 00:16:32 +0100

    Feature change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Everything's awesome!
+          Everything's new and awesome!
         </a>
       </header>
     </div>
